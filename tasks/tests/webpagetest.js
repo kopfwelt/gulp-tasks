b391bf90baa6113fa2f26279ad8a01cf6885008e
Steve Sinnwell <steve@kopfwelt.com>
2015-11-06 07:47:21 +0100

    [add] test for handlebars

--- tasks/tests/webpagetest.js
+++ tasks/tests/webpagetest.js
@@ -1,51 +1,51 @@
-const gulp = require('gulp');
-const webpagetest = require('webpagetest');
+// const gulp = require('gulp');
+// const Webpagetest = require('webpagetest');
 
-gulp.task('test-perf', () => {
-	const wpt = new webpagetest('www.webpagetest.org', 'APIKEY');
-	const parameters = {
-		disableHTTPHeaders: true,
-		'private': true,
-		video: true,
-		location: 'Dulles:Chrome',
-		login: 'admin',
-		password: 'password'
-	};
-	const testSpecs = {
-		runs: {
-			1: {
-				firstView: {
-					SpeedIndex: 1500
-				}
-			}
-		},
-		median: {
-			firstView: {
-				bytesIn: 1000000,
-				visualComplete: 4000
-			}
-		}
-	};
-	return wpt.runTest('http://someurl.com', parameters, function(err, data) {
-		var checkStatus, testID;
-		testID = data.data.testId;
-		checkStatus = function() {
-			return wpt.getTestStatus(testID, function(err, data) {
-				console.log('Status for ' + testID + ': ' + data.data.statusText);
-				if (!data.data.completeTime) {
-					return setTimeout(checkStatus, 5000);
-				} else {
-					return wpt.getTestResults(testID, {
-						specs: testSpecs
-					}, function(err, data) {
-						console.log('http://www.webpagetest.org/result/' + testID + '/');
-						if (err > 0) {
-							return process.exit(1);
-						}
-					});
-				}
-			});
-		};
-		return checkStatus();
-	});
-});
+// gulp.task('test-perf', () => {
+// 	const wpt = new Webpagetest('www.webpagetest.org', 'APIKEY');
+// 	const parameter = {
+// 		disableHTTPHeaders: true,
+// 		private: true,
+// 		video: true,
+// 		location: 'Dulles:Chrome',
+// 		login: 'admin',
+// 		password: 'password'
+// 	};
+// 	const testSpecs = {
+// 		runs: {
+// 			1: {
+// 				firstView: {
+// 					SpeedIndex: 1500
+// 				}
+// 			}
+// 		},
+// 		median: {
+// 			firstView: {
+// 				bytesIn: 1000000,
+// 				visualComplete: 4000
+// 			}
+// 		}
+// 	};
+
+// 	return wpt.runTest('http://someurl.com', parameter, (err, data) => {
+// 		const testID = data.data.testId;
+// 		const checkStatus = () => {
+// 			return wpt.getTestStatus(testID, (err, data) => {
+// 				// console.log('Status for ' + testID + ': ' + data.data.statusText);
+// 				// if (!data.data.completeTime) {
+// 				// 	return setTimeout(checkStatus, 5000);
+// 				// } else {
+// 				// 	return wpt.getTestResults(testID, {
+// 				// 		specs: testSpecs
+// 				// 	}, function(err, data) {
+// 				// 		console.log('http://www.webpagetest.org/result/' + testID + '/');
+// 				// 		if (err > 0) {
+// 				// 			return process.exit(1);
+// 				// 		}
+// 				// 	});
+// 				// }
+// 			});
+// 		};
+// 		return checkStatus();
+// 	});
+// });
---
+++ test/unit/tests/templates/handlebars.test.js
@@ -0,0 +1,62 @@
+const mocha = require('mocha');
+const describe = mocha.describe;
+const it = mocha.it;
+const beforeEach = mocha.beforeEach;
+const afterEach = mocha.afterEach;
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const gulp = require('gulp');
+
+const fs = require('graceful-fs');
+const rimraf = require('rimraf');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+require('../../../../tasks/templates/handlebars');
+
+const outpath = path.join(__dirname, '../../out-fixtures');
+
+const tempFileContent = 'A test generated this file and it is safe to delete';
+
+function createTempFile(path) {
+	fs.writeFileSync(path, tempFileContent);
+}
+
+function updateTempFile(path) {
+	const gazeTimeout = 125;
+	setTimeout(() => {
+		fs.appendFileSync(path, ' changed');
+	}, gazeTimeout);
+}
+
+describe('handlebars gulp task', () => {
+	beforeEach(rimraf.bind(null, outpath));
+	beforeEach(mkdirp.bind(null, outpath));
+	afterEach(rimraf.bind(null, outpath));
+
+	it('should be registered as gulp tasks', () => {
+		expect(gulp.tasks.handlebars).to.be.an('object');
+	});
+
+	it('should compile templates to html', done => {
+		const tempFile = path.join(outpath, 'watch-func.txt');
+
+		createTempFile(tempFile);
+
+		gulp.task('test', cb => {
+			// watcher.close();
+			cb();
+			done();
+		});
+
+		gulp.watch(tempFile, ['handlebars', 'test']);
+
+		updateTempFile(tempFile);
+	});
+
+	it('should change postfix to .html', done => {
+		done();
+	});
+});
