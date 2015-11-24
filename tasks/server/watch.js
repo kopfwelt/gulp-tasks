409fb31bbd84eb245131cc6cac047ec46edf4ad5
Steve Sinnwell <steve@kopfwelt.com>
2015-11-24 18:58:03 +0100

    [add] simple watch task without calling watchTasks

---
+++ tasks/server/watch.js
@@ -0,0 +1,23 @@
+const config = require(`${__configpath}config`);
+const gulp = require('gulp');
+const path = require('path');
+const watch = require('gulp-watch');
+
+const watchTask = () => {
+	// const watchableTasks = ['fonts', 'iconFont', 'images', 'svgSprite','html', 'css'];
+
+	// watchableTasks.forEach(function(taskName) {
+	//   const task = config.tasks[taskName];
+	//   if(task) {
+	//     const glob = path.join(config.root.src, task.src, '**/*.{' + task.extensions.join(',') + '}');
+	//     watch(glob, function() {
+	//      require('./' + taskName)();
+	//     });
+	//   }
+	// });
+	const glob = path.join(config.root.src, '**/*.js,html,xml}');
+	watch(glob);
+};
+
+gulp.task('watch', ['browserSync'], watchTask);
+module.exports = watchTask;
